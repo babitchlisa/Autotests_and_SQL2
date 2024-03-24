9074487ca023abd9237b0f5c45e3f289725c3856
babitchlisa <babitchlisa@yandex.ru>
2024-03-25 00:40:11 +0300

    добавлен скриншот теста

--- order_test.py
+++ order_test.py
@@ -3,6 +3,5 @@ import send_request
 
 def test_order_assert():
     track = send_request.create_order().json()['track']
-    print(track)
     order = send_request.get_order(track)
     assert order.status_code == 200
